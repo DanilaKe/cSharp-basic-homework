594774a55b0fda363a6c91a06bcc80f3504011dc
DanilaK <m98ail@icloud.com>
2018-07-02 21:29:27 +0300

    Func printer

--- Program.cs
+++ Program.cs
@@ -5,8 +5,12 @@ namespace cSharp_basic_homework_1
     class Program
     {
         static void Main(string[] args)
+        { 
+            printer("Hola world!");
+        }
+        static void printer(string message)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(message);
         }
     }
 }
