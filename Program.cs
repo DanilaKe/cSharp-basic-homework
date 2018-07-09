9aa3dac17ff77c30af7e4f10e7b88c2b6d56cd41
DanilaK <m98ail@icloud.com>
2018-07-09 03:37:59 +0300

    solution first problem

--- Program.cs
+++ Program.cs
@@ -5,12 +5,49 @@ namespace cSharp_basic_homework_1
     class Program
     {
         static void Main(string[] args)
-        { 
-            printer("Hola world!");
-        }
-        static void printer(string message)
         {
-            Console.WriteLine(message);
+            int [] ResultOfProblemOne = ProblemOne();
+            Console.WriteLine("Problem One : ");
+            for(int i = 0; i < 3; i++)
+                Console.WriteLine(ResultOfProblemOne[i]);
+        }
+        /*
+        Problem 1.Some Primes
+        Find the 24th, 101st and 251st prime number.
+         */
+        static int[] ProblemOne()
+        {
+            int [] ArrayOfSomePrimeNumbers = new int[3];
+            ArrayOfSomePrimeNumbers[0] = Number.PrimeNumber(24);
+            ArrayOfSomePrimeNumbers[1] = Number.PrimeNumber(101);
+            ArrayOfSomePrimeNumbers[2] = Number.PrimeNumber(251);
+            return ArrayOfSomePrimeNumbers;
+        }
+    }
+    class Number 
+    {
+        static bool isPrime(int n)
+        {
+            if(n <= 1)
+                return false;
+            for(int i = 2; i*i <= n; i++)
+            {
+                if(n%i==0)
+                    return false;
+            }
+            return true;
+        } 
+        public static int PrimeNumber(int Number)
+        {
+            int i = 0;
+            int ResultNumber = 0;
+            while(i < Number)
+            {
+                ResultNumber++;
+                if(isPrime(ResultNumber) == true)
+                    i++;
+            }
+            return ResultNumber;
         }
     }
 }
