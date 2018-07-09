19d6645b6769e2406b4a22647f1b6e43b77ec145
DanilaK <m98ail@icloud.com>
2018-07-10 02:38:34 +0300

    mkdir cd cSharp-basic-homework-2 and new console dotnet app

---
+++ cSharp-basic-homework-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharp_basic_homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
